529369c602b9e499740d67f0fbcf6edb5733cbed
Louis <louis.lesueur@gmx.fr>
2020-05-30 16:24:48 +0200

    no more shallow

--- ndf.py
+++ ndf.py
@@ -8,30 +8,24 @@ import torch.nn.functional as F
 
 
 class MNISTFeatureLayer(nn.Sequential):
-    def __init__(self,dropout_rate,shallow=False):
+    def __init__(self,dropout_rate):
         super(MNISTFeatureLayer, self).__init__()
-        self.shallow = shallow
-        if shallow:
-            self.add_module('conv1', nn.Conv2d(1, 64, kernel_size=15,padding=1,stride=5))
-        else:
-            self.add_module('conv1', nn.Conv2d(1, 32, kernel_size=3, padding=1))
-            self.add_module('relu1', nn.ReLU())
-            self.add_module('pool1', nn.MaxPool2d(kernel_size=2))
-            self.add_module('drop1', nn.Dropout(dropout_rate))
-            self.add_module('conv2', nn.Conv2d(32, 64, kernel_size=3, padding=1))
-            self.add_module('relu2', nn.ReLU())
-            self.add_module('pool2', nn.MaxPool2d(kernel_size=2))
-            self.add_module('drop2', nn.Dropout(dropout_rate))
-            self.add_module('conv3', nn.Conv2d(64, 128, kernel_size=3, padding=1))
-            self.add_module('relu3', nn.ReLU())
-            self.add_module('pool3', nn.MaxPool2d(kernel_size=2))
-            self.add_module('drop3', nn.Dropout(dropout_rate))
+
+        self.add_module('conv1', nn.Conv2d(1, 32, kernel_size=3, padding=1))
+        self.add_module('relu1', nn.ReLU())
+        self.add_module('pool1', nn.MaxPool2d(kernel_size=2))
+        self.add_module('drop1', nn.Dropout(dropout_rate))
+        self.add_module('conv2', nn.Conv2d(32, 64, kernel_size=3, padding=1))
+        self.add_module('relu2', nn.ReLU())
+        self.add_module('pool2', nn.MaxPool2d(kernel_size=2))
+        self.add_module('drop2', nn.Dropout(dropout_rate))
+        self.add_module('conv3', nn.Conv2d(64, 128, kernel_size=3, padding=1))
+        self.add_module('relu3', nn.ReLU())
+        self.add_module('pool3', nn.MaxPool2d(kernel_size=2))
+        self.add_module('drop3', nn.Dropout(dropout_rate))
 
     def get_out_feature_size(self):
-        if self.shallow:
-            return 64*4*4
-        else:
-            return 128*3*3
+        return 128*3*3
 
 
 class Tree(nn.Module):
